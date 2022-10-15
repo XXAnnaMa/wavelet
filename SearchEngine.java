2045496586fb0e126767a2b2a30de34c21ee01d2
Xiaoxiang(Anna) Ma <xim006@ucsd.edu>
2022-10-14 21:39:59 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,55 +2,58 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class Handler implements URLHandler {
-    
-    List<String> list = new ArrayList<>();
-  
+class Handler implements URLHandler
+{
+    ArrayList<String> strlist = new ArrayList<>();
+    String str = "Nothing Show Off!";
+
     public String handleRequest(URI url) {
-
         if (url.getPath().equals("/")) {
-            return "Nothing show off";
+            return str;
         } 
-        else {
+        else{
             System.out.println("Path: " + url.getPath());
-                if(url.getPath(). contains("/add")) {
-                    
-                    String[] parameters = url.getPath().split("=");
-                    if (parameters[0].equals("s")){
-                        
-                        list.add(parameters[1]);
-                        return String.formate(format:"%s has already been added", parameter[1]);
+            if (url.getPath().equals("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    strlist.add(parameters[1]);
+
+                    return String.format("%s has already been added", parameters[1]);
                 }
-                if(url.getPath().contains("/search")) {
-                    String[] parameters = url.getQuery().split("=");
-                    if (parameters[0].equals("s")) {
-                        String empty = " ";
-                        for (String s: list){
-
-                            if (s.contains(parameters[1])){
-
-                                empty += s;
-                                empty += " ";
-                            }
-                        }
-
-                    return empty;
-
+            }
+            else if (url.getPath().equals("/search")) 
+            {
+                System.out.println("Path: " + url.getPath());
+                if (url.getPath().contains("/search")){
+                  String[] parameters = url.getQuery().split("=");
+                  
+                  if (parameters[0].equals("s")){
+                      ArrayList<String> list = new ArrayList<>();
+                      for (String e: strlist){
+                        if (e.contains(parameters[1]))
+                            list.add(e);
+                      }
+                        return String.format("" + list);
+                    }
+                  }
+                }
             }
             return "404 Not Found!";
         }
-    }
 }
+    
 
-class SearchEngine{
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+
+class SearchEngine
+{
+    public static void main(String[] args) throws IOException
+    {
+        if(args.length == 0)
+        {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new Handler());
     }
 }
